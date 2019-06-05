7844db543c8fa8de66a67a427ed0737490217659
Guido Flohr <guido.flohr@cantanea.com>
2019-06-05 14:53:22 +0300

    display menu and errors

--- routes/bank/index.js
+++ routes/bank/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('bank/index', { name: 'Alice' });
+	res.locals.pageType = 'BANK';
+	res.render('bank/index', { name: 'Alice', showMenu: true });
 });
 
 module.exports = router;
--- routes/bank/login.js
+++ routes/bank/login.js
@@ -5,7 +5,14 @@ router.get('/', (req, res, next) => {
 	res.render('bank/login', { title: 'Login' });
 });
 router.post('/', (req, res, next) => {
-	res.send('todo');
+	const body = req.body;
+	if (body.username === 'admin' && body.password === 'admin') {
+		req.session.name = 'admin';
+		res.redirect('/bank');
+	} else {
+		res.render('bank/login', { title: 'Login', errorMsg: 'Login failure!' })
+	}
+	console.log(req.body);
 });
 
 module.exports = router;
