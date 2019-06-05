29795e52604b358d372fd53937f955e4052ffbb8
Guido Flohr <guido.flohr@cantanea.com>
2019-06-05 08:40:51 +0300

    restructured

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/login');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/bank/login');
 
 var app = express();
 
@@ -21,8 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/users', usersRouter);
+app.use('/bank/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/bank/login.js
+++ routes/bank/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('login', { title: 'Login' });
+	res.render('bank/login', { title: 'Login' });
 });
 
 module.exports = router;
